7ee00093335a440f6b997c4bd1a88aac9af9d90d
KarolannMauger <karolannmauger@gmail.com>
2025-10-15 11:13:54 -0400

    feat: Add API Config

--- backend/config.py
+++ backend/config.py
@@ -7,9 +7,10 @@ LED_PIN     = int(os.getenv("LED_PIN", "18"))     # GPIO18 = PWM
 LED_FREQ_HZ = int(os.getenv("LED_FREQ_HZ", "800000"))
 LED_DMA     = int(os.getenv("LED_DMA", "10"))
 LED_INVERT  = os.getenv("LED_INVERT", "0") == "1"
+LED_BRIGHTNESS_BOOT = int(os.getenv("LED_BRIGHTNESS_BOOT", "255"))
 LED_CHANNEL = int(os.getenv("LED_CHANNEL", "0"))
 
 # App
 HOST = os.getenv("HOST", "0.0.0.0")
 PORT = int(os.getenv("PORT", "8000"))
-DEBUG = os.getenv("DEBUG", "1") == "1"
+DEBUG = os.getenv("DEBUG", "0") == "1"
